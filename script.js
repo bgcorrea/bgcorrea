5a624b7d5d974596dbf41488e2a754c28cf5a013
bgcorrea <bgcorrea@uc.cl>
2023-02-07 23:51:32 -0300

    nuevos cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector(".card_profile h1");
+const p = document.querySelector('.card_profile p');
+const twitch = document.querySelector(".twitch")
+
+console.log({
+    h1,
+    p,
+    twitch
+})
