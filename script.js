0443a11bfa6c61a65f4977ec830371692c53878c
bgcorrea <bgcorrea@uc.cl>
2023-02-08 23:28:44 -0300

    nueva versión del input

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 const h1 = document.querySelector(".card_profile h1");
 const p = document.querySelector('.card_profile p');
 const twitch = document.querySelector(".twitch")
+const input = document.querySelector("#inputEnviar")
+const buttonEnviar = document.querySelector("#btnEnviar")
+const resultado = document.querySelector("#resultado")
 
 console.log({
     h1,
     p,
-    twitch
+    twitch,
+    input,
+buttonEnviar
 })
+
+buttonEnviar.addEventListener("click", btnOnClick);
+
+function btnOnClick(){
+    console.log(input.value);
+    resultado.innerText = "¡Su mensaje ha sido enviado con éxito!";
+}
+
+
+
+// h1.innerHTML = "Benjamín Correa Peñaloza";
+
+// h1.getAttribute = "patito"
+// h1.setAttribute = ("class", "rojo")
+// h1.classList.add = ("rojo")
+// h1.classList.remove = ("rojo")
+//toggle y contains
+
+//const img = document.createElement("span")
+//img.setAttribute("src", "https://www.ngenespanol.com/wp-content/uploads/2023/01/puma-el-animal-de-la-fuerza-y-vitalidad-de-los-pueblos-prehispanicos.jpg")
+//console.log(img)
+
+// const email = document.querySelector(".card_information .mail")
+
+// email.appendChild(img);
+
+
+//////////////////////////////////////////
+
+
